c9f07c265db16fdfd2481358c22528e30ff4e39a
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2023-05-02 00:14:41 +0200

    added tracks, and edited AudioPlayerVC

--- Navigation/Modules/AudioPlayer/AudioPlayerViewController.swift
+++ Navigation/Modules/AudioPlayer/AudioPlayerViewController.swift
@@ -16,6 +16,7 @@ class AudioPlayerViewController: UIViewController {
     
     private var audioPlayer: AVAudioPlayer?
     
+    //MARK: - Properties
     private lazy var playPauseButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -32,28 +33,68 @@ class AudioPlayerViewController: UIViewController {
         return button
     }()
     
-    private lazy var nameMusic: UILabel = {
+    private lazy var nameSong: UILabel = {
         let label = UILabel()
-        label.text = "Queen"
+        label.text = Songs().songs[anyIndex].name
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
+    private lazy var nextSong: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "forward.end.fill"), for: .normal)
+        button.addTarget(self, action: #selector(nextMusic), for: .touchUpInside)
+        return button
+    }()
     
+    private lazy var previousSong: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(systemName: "backward.end.fill"), for: .normal)
+        button.addTarget(self, action: #selector(previousMusic), for: .touchUpInside)
+        return button
+    }()
     
+    private lazy var pictureAlbum: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "picturesAlbum")
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var buttonsStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.spacing = 50
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    private var anySong = Bundle.main.url(forResource: "",
+                                          withExtension: "")
+    
+    private var anyIndex = 0
+    
+    //MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .systemBackground
         
         self.setupUI()
+        
+        self.settingsSongs(name: Songs().songs[0].name, format: Songs().songs[0].format)
         self.settingPlayer()
+        
     }
-    
+
+    //MARK: - Methods
     private func settingPlayer() {
         do {
-            guard let fileURL = Bundle.main.url(forResource: "Queen",
-                                                withExtension: "mp3")
+            
+            guard let fileURL = anySong
             else {
                 assertionFailure("File not found")
                 return
@@ -64,31 +105,43 @@ class AudioPlayerViewController: UIViewController {
             
             audioPlayer = try AVAudioPlayer(contentsOf: fileURL, fileTypeHint: AVFileType.mp3.rawValue)
             
-            audioPlayer?.prepareToPlay()
         } catch {
             print(error)
         }
     }
     
     private func setupUI() {
-        view.addSubview(self.playPauseButton)
-        view.addSubview(self.stopButton)
-        view.addSubview(self.nameMusic)
         
-        self.playPauseButton.snp.makeConstraints { maker in
-            maker.centerY.equalToSuperview()
-            maker.left.equalTo(50)
-        }
-        self.stopButton.snp.makeConstraints { maker in
-            maker.centerY.equalToSuperview()
-            maker.right.equalTo(self.view.snp.right).inset(50)
+        view.addSubview(self.pictureAlbum)
+        view.addSubview(self.nameSong)
+        view.addSubview(self.buttonsStackView)
+        self.buttonsStackView.addArrangedSubview(self.previousSong)
+        self.buttonsStackView.addArrangedSubview(self.playPauseButton)
+        self.buttonsStackView.addArrangedSubview(self.stopButton)
+        self.buttonsStackView.addArrangedSubview(self.nextSong)
+        
+        self.pictureAlbum.snp.makeConstraints { maker in
+            maker.width.equalTo(250)
+            maker.height.equalTo(250)
+            maker.centerX.equalToSuperview()
+            maker.centerY.equalTo(self.view.snp.centerY).offset(-100)
         }
         
-        self.nameMusic.snp.makeConstraints { maker in
-            maker.centerY.equalToSuperview()
+        self.nameSong.snp.makeConstraints { maker in
+            maker.top.equalTo(self.pictureAlbum.snp.bottom).offset(50)
             maker.centerX.equalToSuperview()
         }
         
+        self.buttonsStackView.snp.makeConstraints { maker in
+            maker.top.equalTo(self.nameSong.snp.bottom).offset(25)
+            maker.centerX.equalToSuperview()
+        }
+    }
+    
+    private func settingsSongs(name: String, format: String) {
+        
+        anySong = Bundle.main.url(forResource: name,
+                                  withExtension: format)
     }
     
     @objc private func playPauseMusic() {
@@ -107,14 +160,86 @@ class AudioPlayerViewController: UIViewController {
     @objc private func stopMusic() {
         if let player = audioPlayer {
             if player.currentTime > 0 || player.isPlaying == true {
-                player.pause()
+                player.stop()
                 player.currentTime = 0
                 playPauseButton.setImage(UIImage(systemName: "play"), for: .normal)
             }
             else if player.currentTime > 0 || player.isPlaying == false {
                 player.currentTime = 0
+                player.stop()
             }
         }
     }
     
+    @objc private func nextMusic() {
+       
+        if audioPlayer?.isPlaying == true {
+            
+            if anyIndex == 4  {
+                anyIndex = 0
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+                audioPlayer?.play()
+                
+            } else {
+                anyIndex += 1
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+                audioPlayer?.play()
+            }
+        } else {
+            
+            if anyIndex == 4  {
+                anyIndex = 0
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+                
+            } else {
+                anyIndex += 1
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+            }
+        }
+    }
+    
+    @objc private func previousMusic() {
+       
+        if audioPlayer?.isPlaying == true {
+            
+            if anyIndex == 0 {
+                anyIndex = 4
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+                audioPlayer?.play()
+                        
+            } else {
+                anyIndex -= 1
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+                audioPlayer?.play()
+ 
+            }
+        } else {
+            if anyIndex == 0 {
+                anyIndex = 4
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+
+            } else {
+                anyIndex -= 1
+                self.settingsSongs(name: Songs().songs[anyIndex].name, format: Songs().songs[anyIndex].format)
+                self.settingPlayer()
+                self.nameSong.text = Songs().songs[anyIndex].name
+
+            }
+        }
+        
+    }
 }
