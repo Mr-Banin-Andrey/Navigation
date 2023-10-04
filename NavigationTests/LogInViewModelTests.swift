4c42ad155abebc53986cd746e1fa9c51beca150c
Andrey Banin <109150204+Mr-Banin-Andrey@users.noreply.github.com>
2023-10-04 17:53:24 +0200

    refactoring LogInViewModelTests

--- NavigationTests/LogInViewModelTests.swift
+++ NavigationTests/LogInViewModelTests.swift
@@ -10,48 +10,43 @@ import XCTest
 
 final class LogInViewModelTests: XCTestCase {
 
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
+    var viewModel: LogInViewModel!
+    var checkerServiceProtocolMock: CheckerServiceProtocolMock!
+    
+    override func setUp() {
+        viewModel = LogInViewModel()
+        checkerServiceProtocolMock = CheckerServiceProtocolMock()
+    }
 
-//case userIsAuthorized
-//case incorrectPassword
-//case userDoesNotExist
+    override func tearDown() {
+        viewModel = nil
+        checkerServiceProtocolMock = nil
+    }
     
     func testStateIncorrectPassword() {
-        
-        let viewModel = LogInViewModel()
-        let checkerServiceProtocolMock = CheckerServiceProtocolMock()
-        
+           
         viewModel.checkerService = checkerServiceProtocolMock
         checkerServiceProtocolMock.fakeResult = .failure(.unknownError(reason: "The password is invalid or the user does not have a password."))
         viewModel.updateState(viewInput: .singIn(user: LogInUser(login: "", password: "")))
         
-        XCTAssertEqual(viewModel.state, .incorrectPassword) //incorrectPassword
+        XCTAssertEqual(viewModel.state, .incorrectPassword)
     }
     
     func testStateUserDoesNotExist() {
-        let viewModel = LogInViewModel()
-        let checkerServiceProtocolMock = CheckerServiceProtocolMock()
-        
+
         viewModel.checkerService = checkerServiceProtocolMock
         checkerServiceProtocolMock.fakeResult = .failure(.unknownError(reason: "There is no user record corresponding to this identifier. The user may have been deleted."))
         viewModel.updateState(viewInput: .singIn(user: LogInUser(login: "", password: "")))
-        XCTAssertEqual(viewModel.state, .userDoesNotExist) //userDoesNotExist
+        
+        XCTAssertEqual(viewModel.state, .userDoesNotExist)
     }
     
     func testUserIsAuthorized() {
-        let viewModel = LogInViewModel()
-        let checkerServiceProtocolMock = CheckerServiceProtocolMock()
-        
+      
         viewModel.checkerService = checkerServiceProtocolMock
-                
         checkerServiceProtocolMock.fakeResult = .success(UserModel(from: ""))
         viewModel.updateState(viewInput: .willNewUserRegistration(user: LogInUser(login: "", password: "")))
+        
         XCTAssertEqual(viewModel.state, .userIsAuthorized)
     }
     
